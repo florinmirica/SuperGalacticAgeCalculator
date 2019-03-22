abeb97c6933f8a049f3e3438ee5edd3921459494
Florin Mirica <miricaflorin@hotmail.com>
2019-03-22 13:28:51 -0700

    add user logic

--- src/main.js
+++ src/main.js
@@ -4,15 +4,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Calculator from './calculator.js';
 
-$(document).ready( function() {
-    $('form#calculator-form').submit(function(event){
-        event.preventDefault();
-        let dateInput = new Date($('#theDate').val());
-        let day = dateInput.getDate()+1;
-        let month = dateInput.getMonth()+1;
-        let year = dateInput.getFullYear();
-        let date = new Calculator(day, month, year);
-        let result = date.getWeekday();
-        $('#output').prepend(result);
-    });
+import { UserAge } from "./galactic-logic";
+
+$(document).ready(function() {
+  $("#input").submit(function(event) {
+    event.preventDefault();
+    let DOB = $("#inputDOB").val();
+    let lifeExpectancy = parseInt($("#inputExpectancy").val());
+    let newUser = new UserAge(DOB, lifeExpectancy);
+    $("#earthAge").text(newUser.calculateEarthAgeInYears());
+    $("#earthLeft").text(newUser.calculateYearsLeftOnPlanet("Earth"));
+    $("#mercuryAge").text(newUser.calculateAgeOnPlanet("Earth"));
+    $("#mercuryLeft").text(newUser.calculateYearsLeftOnPlanet("Mercury"));
+    $("#venusAge").text(newUser.calculateAgeOnPlanet("Mercury"));
+    $("#venusLeft").text(newUser.calculateYearsLeftOnPlanet("Venus"));
+    $("#marsAge").text(newUser.calculateAgeOnPlanet("Mars"));
+    $("#marsLeft").text(newUser.calculateYearsLeftOnPlanet("Mars"));
+    $("#jupiterAge").text(newUser.calculateAgeOnPlanet("Jupiter"));
+    $("#jupiterLeft").text(newUser.calculateYearsLeftOnPlanet("Jupiter"));
+  });
 });
